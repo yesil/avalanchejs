81d5bda8ad75d8ed5df628ccdaf9075f7269582d
Ilyas Stéphane Türkben <tuerkben@adobe.com>
2021-04-06 21:53:31 +0200

    examples/info folder ok

--- examples/info/getBlockchainID.ts
+++ examples/info/getBlockchainID.ts
@@ -1,20 +1,17 @@
-import { 
-  Avalanche
-} from "../../dist";
-import { InfoAPI } from "../../dist/apis/info";
-  
+import Avalanche from '../../src/Avalanche';
+import { InfoAPI } from '../../src/apis/info';
+
 const ip: string = 'localhost';
 const port: number = 9650;
 const protocol: string = 'http';
 const networkID: number = 12345;
 const avalanche: Avalanche = new Avalanche(ip, port, protocol, networkID);
-const info: InfoAPI = avalanche.Info();
-  
+const info = new InfoAPI(avalanche);
+
 const main = async (): Promise<any> => {
-  const alias: string = "X";
+  const alias: string = 'X';
   const blockchainID : string = await info.getBlockchainID(alias);
   console.log(blockchainID);
-}
-    
-main()
-  
+};
+
+main();
